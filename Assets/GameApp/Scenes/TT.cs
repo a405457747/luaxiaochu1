a58d753bf7d0bb2384dac1fde866f03769b80037
Sky.Allen <894982165@qq.com>
2025-05-11 17:23:19 +0800

    1

---
+++ Assets/GameApp/Scenes/TT.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+        print("wo shi nidie ne ne ne ne ne ne ne ne ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
